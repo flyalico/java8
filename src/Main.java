761befefd552fb7dfb7ba6280756e8573fc69f3d
frankoyalico <FLYALICO@GMAIL.COM>
2020-01-29 14:34:08 -0500

    avance 3

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,19 @@ public class Main {
     public static void main(String[] args) {
         List<User> users = new ArrayList<>();
         //obtener la cantidad de usuarios con una edad mayor a 21
+        //y que username comience con r
         users.add(new User("Rafael", 25));
         users.add(new User("Daniel", 20));
         users.add(new User("Tania", 15));
         users.add(new User("Luisa", 24));
         users.add(new User("Raul",21));
-         int contador = 0;
-        contador = (int)users.stream().filter(user -> user.getAge() < 21).count();
-        System.out.println(contador);
+        long contador = 0;
+       // contador = (int)users.stream().filter(user -> user.getAge() < 21).count();
+        contador =  users.stream()
+                .filter(user -> user.getAge()>18 )
+                .filter(user -> user.getUsername().startsWith("R") )
+                .count();
+         System.out.println(contador);
 
     }
 }
