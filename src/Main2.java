7b908b3e3c59c29d70a3f0dacc8ff8963a6d8c26
frankoyalico <FLYALICO@GMAIL.COM>
2020-02-05 09:13:57 -0500

    avance

--- src/Main2.java
+++ src/Main2.java
@@ -6,13 +6,13 @@ public class Main2 {
         List<User> users = new ArrayList<>();
         //obtener la cantidad de usuarios con una edad mayor a 21
         //y que username comience con r
-        
+
         users.add(new User("Rafael", 25));
         users.add(new User("Daniel", 20));
         users.add(new User("Tania", 15));
         users.add(new User("Luisa", 24));
         users.add(new User("Raul",21));
-
+        users.add(new User("franko", 33));
         users.stream()
                 //.map(user -> user.getUsername())
                 .map(User::getUsername)
