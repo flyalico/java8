218554e0851fadb9ec51a26f3872a223ce7b488c
frankoyalico <FLYALICO@GMAIL.COM>
2020-03-04 11:36:38 -0500

    avance

--- src/Sorted.java
+++ src/Sorted.java
@@ -17,8 +17,7 @@ public class Sorted {
         books.add(new Book("El principito",140));
         books.add(new Book("Spring boot", 75));
         books.add(new Book("docker", 50));
-        books.add(new Book("microservicios", 50));
-        books.add(new Book("react", 50));
+         
         
 		  
         Comparator<Book> comparatorCopies = Comparator.comparing(book -> book.getCopies() );
