e15e24ab53d97bca0567c61d194beb5b755a5a61
frankoyalico <FLYALICO@GMAIL.COM>
2020-03-17 09:59:34 -0500

    avance

--- src/Sorted.java
+++ src/Sorted.java
@@ -19,6 +19,7 @@ public class Sorted {
         books.add(new Book("docker",100)); 
         books.add(new Book("microservicios",100)); 
 		 books.add(new Book("spring cloud",100));  
+		 books.add(new Book("spring boot",100));
         Comparator<Book> comparatorCopies = Comparator.comparing(book -> book.getCopies() );
         Comparator<Book> comparatorTitle = Comparator.comparing(book -> book.getTitle());
 
